ea0c4cd317426f563fb3e5de794a160579465de1
beniiiii <am42310@ens.etsmtl.ca>
2016-02-20 20:45:30 -0500

    Bonhomme qui bouge et suit la souris

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+    Vector3 lookPos;
+    Rigidbody rigidBody;
+
+    public float Speed = 4;
+
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit hit;
+
+        if(Physics.Raycast(ray, out hit, 100))
+        {
+            lookPos = hit.point;
+        }
+
+        Vector3 lookDirection = lookPos - transform.position;
+        lookDirection.y = 0;
+
+        transform.LookAt(transform.position + lookDirection, Vector3.up);
+    }
+
+    void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(horizontal, 0, vertical);
+
+        rigidBody.AddForce(movement * Speed / Time.deltaTime);
+
+    }
+}
