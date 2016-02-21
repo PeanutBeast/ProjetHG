ceb1f13d1af2457a56004effd9849318b17f3703
beniiiii <am42310@ens.etsmtl.ca>
2016-02-20 23:13:19 -0500

    Ajout des balles lors du clic de la souris

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,10 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
 public class PlayerMovement : MonoBehaviour {
 
     Vector3 lookPos;
     Rigidbody rigidBody;
+
+    public GameObject bullet;
+    public float BulletSpeed = 10;
 
     public float Speed = 4;
 
@@ -15,19 +19,8 @@ public class PlayerMovement : MonoBehaviour {
 
     void Update()
     {
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-
-        RaycastHit hit;
-
-        if(Physics.Raycast(ray, out hit, 100))
-        {
-            lookPos = hit.point;
-        }
-
-        Vector3 lookDirection = lookPos - transform.position;
-        lookDirection.y = 0;
-
-        transform.LookAt(transform.position + lookDirection, Vector3.up);
+        TurnPlayerWithMouse();
+        ShootBullets();
     }
 
     void FixedUpdate()
@@ -39,5 +32,38 @@ public class PlayerMovement : MonoBehaviour {
 
         rigidBody.AddForce(movement * Speed / Time.deltaTime);
 
+    }
+
+    private void TurnPlayerWithMouse()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, 100))
+        {
+            lookPos = hit.point;
+        }
+
+        Vector3 lookDirection = lookPos - transform.position;
+        lookDirection.y = 0;
+
+        transform.LookAt(transform.position + lookDirection, Vector3.up);
+    }
+
+    private void ShootBullets()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+
+            Quaternion rotation = transform.rotation;
+
+            // Instantiate the projectile at the position and rotation of this transform
+            GameObject clone;
+            clone = (GameObject)Instantiate(bullet, transform.position, rotation);
+
+            // Add force to the cloned object in the object's forward direction
+            clone.GetComponent<Rigidbody>().AddForce(clone.transform.forward * BulletSpeed);
+        }
     }
 }
